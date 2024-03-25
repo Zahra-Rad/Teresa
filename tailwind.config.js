c333c3d722a1f13f060265f0b867cf63ef5bbaee
Zahra Kaboodvandi Rad <118894293+Zahra-Rad@users.noreply.github.com>
2024-03-25 18:21:05 +0430

    added logo section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
       backgroundImage: {
         bg: "url('../../img/bg.png')",
       },
+      borderRadius: {
+        none: "0",
+        sm: "0.125rem",
+        DEFAULT: "0.25rem",
+        DEFAULT: "4px",
+        md: "0.375rem",
+        lg: "0.5rem",
+        full: "9999px",
+        large: "12px",
+        primary: "33% 67% 32% 68% / 32% 28% 72% 68%",
+      },
+      animation: {
+        format: "format 10s ease-in-out infinite alternate",
+        format2:"format2 10s ease-in-out infinite alternate",
+      },
     },
   },
   plugins: [],
